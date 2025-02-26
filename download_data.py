33b7bb49bae9dd4aa0e24c789f3e03d110e317e4
XwaeK <fabien.lagnieu@yahoo.fr>
2025-02-26 13:48:56 +0100

    val

--- download_data.py
+++ download_data.py
@@ -1,169 +1,40 @@
-"""Fetcher for RAMP data stored in OSF
+import requests
+import os
 
-To adapt it for another challenge, change the CHALLENGE_NAME and upload
-public/private data as `tar.gz` archives in dedicated OSF folders named after
-the challenge.
-"""
-import shutil
-import tarfile
-import argparse
-from zlib import adler32
-from pathlib import Path
-from osfclient.api import OSF
-from osfclient.exceptions import UnauthorizedException
+# Liens vers les trois datasets
+datasets = [
+    "https://www.data.gouv.fr/fr/datasets/r/d7e5740b-9c28-4caa-9801-8354390a9bcb",
+    "https://www.data.gouv.fr/fr/datasets/r/5d8dc837-ff82-420f-9130-5ea456293288",
+    "https://www.data.gouv.fr/fr/datasets/r/cc19ff95-8ff8-43b7-a180-c84258a5c0c3",
+    "https://www.data.gouv.fr/fr/datasets/r/90d589c8-0849-4392-852c-78bfcd820785"
+]
 
-CHALLENGE_NAME = 'map_estimation'
-PUBLIC_PROJECT = "t4uf8"
-PRIVATE_PROJECT = "g5t7q"
-PRIVATE_CKSUM = 2716027363
-PUBLIC_CKSUM = 2171499960
+# Dossier de destination pour les fichiers téléchargés
+download_folder = "data"
 
+file_names = ["interventions-hebdo-2010-2017.csv", "91-variability-features.csv", "91-communes-features.csv", "interventions-sdis91.csv"]
 
-def get_folder(code, username=None, password=None):
-    "Get connection to OSF and info relative to public/private data."
-    # Get the connection to OSF and find the folder in the OSF project
-    osf = OSF(username=username, password=password)
+# Créer le dossier si nécessaire
+os.makedirs(download_folder, exist_ok=True)
 
-    try:
-        project = osf.project(code)
-        store = project.storage('osfstorage')
-    except UnauthorizedException:
-        raise ValueError("Invalid credentials for RAMP private storage.")
-    return get_one_element(store.folders, CHALLENGE_NAME)
+def download_file(url, file_name, dest_folder):
+    # Extraire le nom du fichier depuis l'URL
+    filename = file_name
+    filepath = os.path.join(dest_folder, filename)
 
+    # Télécharger le fichier
+    print(f"Téléchargement de {url}...")
+    response = requests.get(url)
+    response.raise_for_status()  # Vérifier si la requête a réussi
 
-def get_one_element(container, name):
-    "Get one element from OSF container with a comprehensible failure error."
-    elements = [f for f in container if f.name == name]
-    container_name = (
-        container.name if hasattr(container, 'name') else CHALLENGE_NAME
-    )
-    assert len(elements) == 1, (
-        f'There is no element named {name} in {container_name} from the RAMP '
-        'OSF account.'
-    )
-    return elements[0]
+    # Sauvegarder le fichier localement
+    with open(filepath, 'wb') as f:
+        f.write(response.content)
+    print(f"Fichier téléchargé et sauvegardé sous {filepath}")
 
 
-def hash_folder(folder_path):
-    """Return the Adler32 hash of an entire directory."""
-    folder = Path(folder_path)
+# Télécharger tous les datasets
+for url, file_name in zip(datasets, file_names):
+    download_file(url, file_name, download_folder)
 
-    # Recursively scan the folder and compute a checksum
-    checksum = 1
-    for f in sorted(folder.rglob('*.h5')):
-        checksum = adler32(f.read_bytes(), checksum)
-
-    return checksum
-
-
-def checksum_data(data_dir, cksum, raise_error=False):
-    print("Checking the data...", end='', flush=True)
-    local_checksum = hash_folder(data_dir)
-    if raise_error and cksum != local_checksum:
-        raise ValueError(
-            f"The checksum does not match. Expecting {cksum} but "
-            f"got {local_checksum}. The archive seems corrupted. Try to "
-            f"remove {data_dir} and re-run this command."
-        )
-    print("Done.")
-
-    return local_checksum == cksum
-
-
-def download_split_archive_from_osf(folder, split_files, data_dir):
-    """
-    Merge split files, extract the tar.gz file, and delete the merged file.
-
-    Args:
-        split_files (list of Path): Ordered list of split file paths.
-        extract_path (str): Directory where files will be extracted.
-    """
-    output_tar = (data_dir / split_files[0]).with_suffix("")
-    assert "001" not in output_tar.name, output_tar.name
-
-    N = len(split_files)
-    with open(output_tar, 'wb') as f:
-        for i, fname in enumerate(split_files):
-            print(f"Downloading {output_tar.name} ({i+1}/{N})...\r")
-            osf_file = get_one_element(folder.files, fname)
-            osf_file.write_to(f)
-    print("Downloading done.".ljust(40))
-    return output_tar
-
-
-def download_from_osf(folder, filename, data_dir):
-    # Download the archive in the data
-    target_path = data_dir / filename
-    osf_file = get_one_element(folder.files, filename)
-    print(f"Downloading {filename}...\r", end='', flush=True)
-    with open(target_path, 'wb') as f:
-        osf_file.write_to(f)
-    print("Downloading done.".ljust(40))
-    return target_path
-
-
-def setup_data(data_path, private=False, username=None, password=None):
-    "Download and uncompress the data from OSF."
-    data_path = Path(data_path)
-    cksum = PRIVATE_CKSUM if private else PUBLIC_CKSUM
-
-    if not data_path.exists() or cksum is None:
-        data_path.mkdir(exist_ok=True)
-    elif checksum_data(data_path, cksum, raise_error=False):
-        print("Data already downloaded and verified.")
-        return
-
-    # Download the public data
-    split_files = [
-        'public.tar.gz.001', 'public.tar.gz.002', 'public.tar.gz.003'
-    ]
-    public_folder = get_folder(PUBLIC_PROJECT)
-    archive = download_split_archive_from_osf(
-        public_folder, split_files, data_path
-    )
-    print("Extracting the data...", end='', flush=True)
-    with tarfile.open(archive) as tar:
-        tar.extractall(data_path)
-
-    # Remove intermediate folder public and make a copy of test.h5
-    # for public validation.h5
-    [f.rename(data_path / f.name)
-        for f in (data_path / "public").glob("*")]
-    (data_path / "public").rmdir()
-    archive.unlink()
-    shutil.copyfile(data_path / "test.h5", data_path / "validation.h5")
-    print("Done.")
-
-    if private:
-        private_folder = get_folder(PRIVATE_PROJECT, username, password)
-        test = download_from_osf(
-            private_folder, "private_test.h5", data_path
-        )
-        validation = download_from_osf(private_folder, "test.h5", data_path)
-        validation.rename(data_path / "validation.h5")
-        test.rename(data_path / "test.h5")
-
-    checksum_data(data_path, cksum, raise_error=True)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=f'Data loader for the {CHALLENGE_NAME} challenge on RAMP.'
-    )
-    parser.add_argument('--data-path', type=Path, default=Path("data"),
-                        help='If this flag is used, download the private data '
-                        'from OSF. This requires the username and password '
-                        'options to be provided.')
-    parser.add_argument('--private', action="store_true",
-                        help='If this flag is used, download the private data '
-                        'from OSF. This requires the username and password '
-                        'options to be provided.')
-    parser.add_argument('--username', type=str, default=None,
-                        help='Username for downloading private OSF data.')
-    parser.add_argument('--password', type=str, default=None,
-                        help='Password for downloading private OSF data.')
-    args = parser.parse_args()
-
-    setup_data(args.data_path, private=args.private,
-               username=args.username, password=args.password)
+print("Tous les fichiers ont été téléchargés avec succès.")
--- problem.py
+++ problem.py
@@ -1,12 +1,13 @@
 import rampwf as rw
-
+import warnings
+warnings.filterwarnings('ignore')  
 import os
 import h5py
 import numpy as np
 import pandas as pd
 from pathlib import Path
 
-problem_title = 'MAP estimation from non-invasive monitoring'
+problem_title = 'sdis91-estimation'
 
 # A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.make_regression()
@@ -38,8 +39,13 @@ def get_cv(X, y):
     # Make sure the index is a range index so it is compatible with sklearn API
     X = X.reset_index(drop=True)
 
+    chunks = X['chunk'].fillna('t')
+
     def split():
-        yield X.query("chunk != 'val'").index, X.query("chunk == 'val'").index
+        train_idx = chunks[chunks != 'val'].index
+        val_idx = chunks[chunks == 'val'].index
+        yield train_idx, val_idx
+        #yield X.query("chunk != 'val'").index, X.query("chunk == 'val'").index
 
     return split()
 
@@ -108,3 +114,168 @@ def get_test_data(path='.', start=None, stop=None, load_waveform=True):
         start, stop = 0, 100
     rw.X_TEST, rw.Y_TEST = _load_data(file, start, stop, load_waveform)
     return rw.X_TEST, rw.Y_TEST
+
+def preprocess():
+    # Charger les fichiers CSV
+    print("Chargement des fichiers CSV...")
+    interventions_features = pd.read_csv('data/interventions-hebdo-2010-2017.csv', sep=';')
+    interventions_features_test = pd.read_csv('data/interventions-sdis91.csv', encoding='ISO-8859-1', sep=';')  
+    communes_features = pd.read_csv('data/91-communes-features.csv')
+    variability_features = pd.read_csv('data/91-variability-features.csv')
+    print("   -> Fichiers CSV chargés avec succès.")
+
+    print("Traitement des données...")
+    print("   - Interventions_features")
+    # Suppression des lignes qui n'ont que des valeurs NaN dans interventions_features
+    interventions_features = interventions_features.dropna(how='all')
+
+    # Fonction de mise en forme de la date
+    def mise_en_forme_date(df):
+        df['date'] = df['ope_annee'] * 100 + df['ope_semaine']
+        df = df.drop(columns=['ope_annee', 'ope_semaine'])
+        df = df[['date'] + [col for col in df.columns if col != 'date']]
+        return df
+
+    interventions_features = mise_en_forme_date(interventions_features)
+    interventions_features_test = mise_en_forme_date(interventions_features_test)
+
+    # Remplacer les valeurs NAN par 'AUTR' dans interventions_features_test
+    interventions_features_test['ope_categorie'] = interventions_features_test['ope_categorie'].fillna('AUTR')
+
+    # Création d'un DataFrame avec les communes et leur code INSEE
+    communes = pd.DataFrame(columns=['code_insee', 'nom_commune'])
+    communes_list = []
+
+    for code_insee in interventions_features_test['ope_code_insee'].unique():
+        nom_commune = interventions_features_test[interventions_features_test['ope_code_insee'] == code_insee]['ope_nom_commune'].unique()[0]
+        communes_list.append({'code_insee': code_insee, 'nom_commune': nom_commune})
+
+    communes = pd.concat([communes, pd.DataFrame(communes_list)], ignore_index=True)
+    communes['code_insee'] = communes['code_insee'].astype(int)
+
+    # Elaboration du training set et du test set
+    X_train = pd.DataFrame(columns=['semaine', 'code_insee'])
+    X_test = pd.DataFrame(columns=['semaine', 'code_insee'])
+
+    # X_train avec l'ensemble des valeurs (2010-2017) d'interventions_features
+    for date in interventions_features['date'].unique():
+        for code_insee in interventions_features_test['ope_code_insee'].unique():
+            new_row = pd.DataFrame({'semaine': [date], 'code_insee': [code_insee]})
+            X_train = pd.concat([X_train, new_row], ignore_index=True)
+
+    # X_test avec les valeurs de 2018 d'interventions_features_test
+    for date in interventions_features_test['date'].unique():
+        for code_insee in interventions_features_test['ope_code_insee'].unique():
+            if date >= 201801 and date <= 201852:
+                new_row = pd.DataFrame({'semaine': [date], 'code_insee': [code_insee]})
+                X_test = pd.concat([X_test, new_row], ignore_index=True)
+
+    X_train['semaine'] = X_train['semaine'].astype(int)
+    X_train['code_insee'] = X_train['code_insee'].astype(int)
+    X_test['semaine'] = X_test['semaine'].astype(int)
+    X_test['code_insee'] = X_test['code_insee'].astype(int)
+
+    # Fusionner X_train/X_test et communes pour ajouter le nom de la commune
+    X_train = pd.merge(X_train, communes, on='code_insee', how='left')
+    X_test = pd.merge(X_test, communes, on='code_insee', how='left')
+
+    # Création des "target" y_train et y_test
+    type_ope = ['SUAP', 'INCN', 'INCU', 'ACCI', 'AUTR']
+
+    y_train = X_train[['semaine', 'code_insee']]
+    y_test = X_test[['semaine', 'code_insee']]
+
+    def implement_target(df, y):
+        for t in type_ope:
+            y['nb_ope_' + t] = 0
+
+        for date in df['date'].unique():
+            for code_insee in df['ope_code_insee'].unique():
+                for t in type_ope:
+                    df_ = df[(df['date'] == date) & (df['ope_code_insee'] == code_insee)]
+
+                    if not df_.empty:
+                        matching_rows = df_[df_['ope_categorie'] == t]
+                        if not matching_rows.empty:
+                            y.loc[(y['semaine'] == date) & (y['code_insee'] == code_insee), 'nb_ope_' + t] = matching_rows['nb_ope'].values[0]
+                        else:
+                            y.loc[(y['semaine'] == date) & (y['code_insee'] == code_insee), 'nb_ope_' + t] = 0
+                    else:
+                        y.loc[(y['semaine'] == date) & (y['code_insee'] == code_insee), 'nb_ope_' + t] = 0
+        return y
+
+    y_train = implement_target(interventions_features, y_train)
+    y_test = implement_target(interventions_features_test, y_test)
+
+    # Suppression des colonnes devenues inutiles
+    y_train = y_train.drop(columns=['semaine', 'code_insee'])
+    y_test = y_test.drop(columns=['semaine', 'code_insee'])
+    print("      -> interventions_features processed successfully.")
+
+    print("   - Merging communes_features...")
+    # Vérification et intégration des données de communes_features
+    if not communes_features['code_insee'].isin(X_train['code_insee']).all():
+        print("Le fichier communes_features contient des communes non présentes dans X_train")
+
+    # Création des colonnes dans X_train et X_test pour les communes_features
+    for col in communes_features.columns:
+        X_train['com_' + col] = np.nan
+        X_test['com_' + col] = np.nan
+
+    # Remplir les colonnes avec les informations de communes_features
+    for date in communes_features['annee'].unique():
+        for code_insee in communes_features['code_insee'].unique():
+            df = communes_features[(communes_features['annee'] == date) & (communes_features['code_insee'] == code_insee)]
+
+            if not df.empty:
+                for col in communes_features.columns:
+                    if communes_features[col].dtype == 'object':
+                        X_train.loc[(X_train['semaine'] // 100 == date) & (X_train['code_insee'] == code_insee), 'com_' + col] = str(df[col].values[0])
+                        X_test.loc[(X_test['semaine'] // 100 == date) & (X_test['code_insee'] == code_insee), 'com_' + col] = str(df[col].values[0])
+                    else:
+                        X_train.loc[(X_train['semaine'] // 100 == date) & (X_train['code_insee'] == code_insee), 'com_' + col] = df[col].values[0]
+                        X_test.loc[(X_test['semaine'] // 100 == date) & (X_test['code_insee'] == code_insee), 'com_' + col] = df[col].values[0]
+
+    print("      -> communes_features merged successfully.")
+    print("   - Merging variability_features...")
+    # Traitement des données de variability_features
+    variability_features = variability_features.dropna(subset=['pollution_code_insee'])
+
+    if not variability_features['pollution_code_insee'].isin(X_train['code_insee']).all():
+        print("Le fichier des variability_features contient des communes non présentes dans X_train")
+
+    variability_features['date'] = variability_features['cal_annee'] * 100 + variability_features['cal_semaine']
+    variability_features = variability_features.rename(columns={'pollution_code_insee': 'code_insee'})
+
+    for col in variability_features.columns:
+        if variability_features[col].dtype == 'float64':
+            if variability_features[col].apply(lambda x: x.is_integer()).all():
+                variability_features[col] = variability_features[col].astype(int)
+
+    for col in variability_features.columns:
+        X_train['var_' + col] = np.nan
+        X_test['var_' + col] = np.nan
+
+    # Remplir les colonnes avec les informations de variability_features
+    for date in variability_features['date'].unique():
+        for code_insee in variability_features['code_insee'].unique():
+            df = variability_features[(variability_features['date'] == date) & (variability_features['code_insee'] == code_insee)]
+
+            if not df.empty:
+                for col in variability_features.columns:
+                    if variability_features[col].dtype == 'object':
+                        X_train.loc[(X_train['semaine'] == date) & (X_train['code_insee'] == code_insee), 'var_' + col] = str(df[col].values[0])
+                        X_test.loc[(X_test['semaine'] == date) & (X_test['code_insee'] == code_insee), 'var_' + col] = str(df[col].values[0])
+                    else:
+                        X_train.loc[(X_train['semaine'] == date) & (X_train['code_insee'] == code_insee), 'var_' + col] = df[col].values[0]
+                        X_test.loc[(X_test['semaine'] == date) & (X_test['code_insee'] == code_insee), 'var_' + col] = df[col].values[0]
+    print("      -> variability_features merged successfully.")
+
+    # Sauvegarder les datasets traités en H5
+    X_train.to_hdf('data/X_train.h5', key='data', mode='w')
+    X_test.to_hdf('data/X_test.h5', key='data', mode='w')
+    y_train.to_hdf('data/y_train.h5', key='target', mode='w')
+    y_test.to_hdf('data/y_test.h5', key='target', mode='w')
+
+    print("Preprocessing and data merging completed successfully !")
+    print("-> Datasets saved in 'data' folder as X_train.h5, X_test.h5, y_train.h5 and y_test.h5")
