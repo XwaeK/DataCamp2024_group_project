7372f6028fd148bd4074bfa5a82a3de2eff1df27
waddason <tr.waddington@gmail.com>
2025-02-28 17:52:17 +0100

    FIX clean estimator

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -4,16 +4,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import FunctionTransformer
 from sklearn import set_config
 from sklearn.base import BaseEstimator, TransformerMixin
-import pandas as pd
 
 set_config(transform_output="pandas")
-_label_names = [
-    "nb_ope_SUAP",
-    "nb_ope_INCN",
-    "nb_ope_INCU",
-    "nb_ope_ACCI",
-    "nb_ope_AUTR",
-]
 
 
 def preprocess_data(X, n_columns=10):
@@ -39,30 +31,8 @@ class MultiOutputRegressorTransformer(BaseEstimator, TransformerMixin):
         return self.estimator.predict(X)
 
 
-class MyEstimator:
-    def __init__(self):
-        self.pipe = make_pipeline(
-            FunctionTransformer(preprocess_data),
-            MultiOutputRegressorTransformer(
-                RandomForestRegressor(n_estimators=50)
-            ),
-        )
-        self.column_names = _label_names
-
-    def fit(self, X, y):
-        self.pipe.fit(X, y)
-        # print(f"Debug: fit on {X.shape}/{y.shape}")
-        return self
-
-    def predict(self, X):
-        pred = self.pipe.predict(X)
-        # pred_df = pd.DataFrame(pred, columns=self.column_names)
-
-        # print(f"Debug: has predicted {pred_df.shape} {pred_df.columns}")
-        # return pred_df
-        return pred
-
-
 def get_estimator():
-    model = MyEstimator()
-    return model
+    return make_pipeline(
+        FunctionTransformer(preprocess_data),
+        MultiOutputRegressorTransformer(RandomForestRegressor(n_estimators=50)),
+    )
