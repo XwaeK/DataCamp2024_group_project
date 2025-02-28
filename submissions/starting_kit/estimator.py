1e09b337c44bd04ace089fdacbb9c7b26263a3aa
waddason <tr.waddington@gmail.com>
2025-02-28 18:40:10 +0100

    FIX notebook estimator

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import FunctionTransformer
 from sklearn import set_config
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
 
 set_config(transform_output="pandas")
 
@@ -17,18 +18,20 @@ def preprocess_data(X, n_columns=10):
 
 
 class MultiOutputRegressorTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, estimator):
-        self.estimator = MultiOutputRegressor(estimator)
+    def __init__(self, estimator=None):
+        self.estimator = estimator
 
     def fit(self, X, y):
-        self.estimator.fit(X, y)
-        return self
-
-    def transform(self, X):
-        return self.estimator.predict(X)
+        if self.estimator is None:
+            raise ValueError("Estimator cannot be None")
+        self.estimator_ = MultiOutputRegressor(self.estimator)
+        self.estimator_.fit(X, y)
+        check_is_fitted(self, 'estimator_')
+        return self.estimator_.predict(X)
 
     def predict(self, X):
-        return self.estimator.predict(X)
+        check_is_fitted(self, 'estimator_')
+        return self.estimator_.predict(X)
 
 
 def get_estimator():
